d7e86879d686710b8d7485859241e3ddac287178
나병현 <nbhyun0329@gmail.com>
2024-09-25 16:52:05 +0900

    ✨ feat: initialize WebSocket server with client management and message broadcasting

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
 )
+
+var (
+	clients   = make(map[*websocket.Conn]bool)
+	broadcast = make(chan Message)
+	upgrader  = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	mu sync.Mutex
+)
+
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
 
 func main() {
 	fmt.Println("WebSocket")
-	os.Exit(0)
 }
